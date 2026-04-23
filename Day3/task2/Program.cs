d2915e621bf2c7405c739ed73f55c73df7a4f6f2
Valentina <pronskayvalentina127@gmail.com>
2026-04-23 11:14:36 +0300

    Update Program.cs

--- Day3/task2/Program.cs
+++ Day3/task2/Program.cs
@@ -3,7 +3,7 @@ static class ArrayOperations
 {
     public static double Product(double[] arr)
     {
-        double result = 1;
+        double result = 1;//начальное значение
         for (int i = 0; i < arr.Length; i++)
             result *= arr[i];
         return result;
