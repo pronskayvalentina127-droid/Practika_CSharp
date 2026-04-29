c988096b7b0d57f5f307922a9aca2948f31d464e
Valentina <pronskayvalentina127@gmail.com>
2026-04-29 09:15:46 +0300

    Update MyFixedQueue.cs

--- Day8/task2/MyFixedQueue.cs
+++ Day8/task2/MyFixedQueue.cs
@@ -1,13 +1,13 @@
-﻿class MyFixedQueue<T>
+﻿class MyFixedQueue<T> // <T> — обобщение (работает с любым типом)
 {
-    private T[] items;
-    private int head;
-    private int count;
+    private T[] items; // массив для хранения элементов
+    private int head; //индекс самого старого элемента
+    private int count; //кол-во элементов сейчас
     public MyFixedQueue(int size)
     {
-        items = new T[size];
+        items = new T[size]; //массив фиксированого размера
     }
-    public void Enqueue(T item)
+    public void Enqueue(T item) //Enqueue — добавление (с автоматическим удалением старого)
     {
         int index = (head + count) % items.Length;
         items[index] = item;
@@ -16,7 +16,7 @@
         else
             head = (head + 1) % items.Length;
     }
-    public T Dequeue()
+    public T Dequeue() //Dequeue — извлечение первого элемента
     {
         T result = items[head];
         head = (head + 1) % items.Length;
