1b502760da62bfa3eb8fc9211e72cc588a6ac2c8
Valentina <pronskayvalentina127@gmail.com>
2026-04-29 09:08:36 +0300

    Update ElevatorRequest.cs

--- Day8/task1/ElevatorRequest.cs
+++ Day8/task1/ElevatorRequest.cs
@@ -1,11 +1,11 @@
 ﻿class ElevatorRequest
 {
-    public int FloorNumber;
-    public string Direction;
+    public int FloorNumber; // номер этажа
+    public string Direction;  // направление
     public ElevatorRequest(int floor, string dir)
     {
-        FloorNumber = floor;
-        Direction = dir;
+        FloorNumber = floor; //сохраняем этаж 
+        Direction = dir; //направление
     }
 }
 
