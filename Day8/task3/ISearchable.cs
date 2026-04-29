72cddd238db024e4728ab25b7b0c4d193c54d8b3
Valentina <pronskayvalentina127@gmail.com>
2026-04-29 09:20:25 +0300

    Update ISearchable.cs

--- Day8/task3/ISearchable.cs
+++ Day8/task3/ISearchable.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-interface ISearchable<T> // любой перебираемый набор (массив, список).
+interface ISearchable<T> // обобщение, тип неизвестен, любой перебираемый набор (массив, список).
 {
-    T Find(IEnumerable<T> items, Func<T, bool> predicate);//принимает элемент, возвращает true/false
+    T Find(IEnumerable<T> items, Func<T, bool> predicate); //принимает элемент, возвращает true/false
 }
 
 //обощенный-класс
