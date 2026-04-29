c4fd4d821d1e33c169f1c93b66c9b35f4357bbf8
Valentina <pronskayvalentina127@gmail.com>
2026-04-29 09:10:47 +0300

    Update ElevatorSystem.cs

--- Day8/task1/ElevatorSystem.cs
+++ Day8/task1/ElevatorSystem.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 class ElevatorSystem
 {
-    private Queue queue = new Queue();
+    private Queue queue = new Queue(); //создание неджерик очереди (хранит объект)
     public void AddRequest(int floor, string dir)
     {
-        queue.Enqueue(new ElevatorRequest(floor, dir));
+        queue.Enqueue(new ElevatorRequest(floor, dir)); //Enqueue добавляет элемент в конец очереди
     }
     public void ProcessRequests()
     {
         while (queue.Count > 0)
         {
-            ElevatorRequest req = (ElevatorRequest)queue.Dequeue();
+            ElevatorRequest req = (ElevatorRequest)queue.Dequeue();// Dequeue() извлекает первый элемент очереди и удаляет его
             Console.WriteLine($"Этаж: {req.FloorNumber}, {req.Direction}");
         }
     }
