613e5f1210bd964da7c2441d4d72099255ab7d24
Valentina <pronskayvalentina127@gmail.com>
2026-04-29 09:22:43 +0300

    Update SearchManager.cs

--- Day8/task3/SearchManager.cs
+++ Day8/task3/SearchManager.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 class SearchManager<T>
 {
-    private ISearchable<T> search;
+    private ISearchable<T> search; //экземляр класса
     public SearchManager()
     {
         search = new SimpleSearch<T>();
     }
-    public T FindElement(T[] items, Func<T, bool> predicate)
+    public T FindElement(T[] items, Func<T, bool> predicate) //метод поиска элементов
     {
-        return search.Find(items, predicate);
+        return search.Find(items, predicate); //метод Find из SimpleSearch
     }
-    public void DisplaySearchResult(T item)
+    public void DisplaySearchResult(T item) //метод для вывода результатов
     {
         if (item != null)
             Console.WriteLine($"Найден: {item}");
